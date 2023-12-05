b68083651629b7d49655925724ac697ce6d9d5ad
UCLA-Adam <105705018+UCLA-Adam@users.noreply.github.com>
2023-12-04 17:32:53 -0800

    Update test.py

--- test.py
+++ test.py
@@ -145,6 +145,8 @@ while True:
             # call color function
             #
             #
+            pts = np.array([code.polygon], np.int32)
+            cv2.polylines(img, [pts], True, (0, 255, 0), 3)
             cv2.putText(img, str(decoded_data), (rect_pts[0], rect_pts[1]), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
 
     # Update readings and round them
